b6c2a094d9a4e541c0e706856d21bade31e204f4
oleksandr <tychyna.o@gmail.com>
2018-01-14 22:02:27 +0200

    update(*): start project

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+import SpriteSheet from "./SpriteSheet.js";
+import {
+    loadImage
+} from "./loaders.js";
+
+
+
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+
+
+
+loadImage("/img/tiles.png").
+then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define("ground", 0, 0);
+    sprites.define("sky", 3, 23);
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 0; y < 14; ++y) {
+            sprites.drawTile("sky", context, x, y);
+        }
+    }
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 12; y < 14; ++y) {
+            sprites.drawTile("ground", context, x, y);
+        }
+    }
+
+
+});
